3a0fb8147359aa41855f4577878dc8b642a2610b
v03413 <admin@qzone.work>
2026-01-04 11:33:47 +0800

    chore: rename

--- main/main.go
+++ main/main.go
@@ -5,20 +5,20 @@ import (
 	"os"
 
 	"github.com/urfave/cli/v3"
-	"github.com/v03413/bepusdt/app/command"
+	"github.com/v03413/bepusdt/app/cmd"
 )
 
 func main() {
-	cmd := &cli.Command{
+	c := &cli.Command{
 		Name:  "BEpusdt",
 		Usage: "一款更好用的个人加密货币收款网关",
 		Commands: []*cli.Command{
-			command.Start,
-			command.Version,
-			command.Reset,
+			cmd.Start,
+			cmd.Version,
+			cmd.Reset,
 		},
 	}
-	if err := cmd.Run(context.Background(), os.Args); err != nil {
+	if err := c.Run(context.Background(), os.Args); err != nil {
 		panic(err)
 	}
 }
