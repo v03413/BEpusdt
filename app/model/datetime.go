8c4ead016904095475a482a899f99678692e5759
v03413 <admin@qzone.work>
2026-02-27 15:20:34 +0800

    fix: ensure Datetime uses local time zone

--- app/model/datetime.go
+++ app/model/datetime.go
@@ -6,17 +6,18 @@ import (
 	"time"
 )
 
+var localLocation = time.Local
+
 type Datetime time.Time
 
 func (d *Datetime) MarshalJSON() ([]byte, error) {
-	tTime := time.Time(*d)
+	tTime := time.Time(*d).In(localLocation)
 	return []byte(fmt.Sprintf("\"%v\"", tTime.Format("2006-01-02 15:04:05"))), nil
 }
 
 func (d Datetime) Value() (driver.Value, error) {
 	var zeroTime time.Time
 	tlt := time.Time(d)
-	// 判断给定时间是否和默认零时间的时间戳相同
 	if tlt.UnixNano() == zeroTime.UnixNano() {
 		return nil, nil
 	}
@@ -26,34 +27,27 @@ func (d Datetime) Value() (driver.Value, error) {
 func (d *Datetime) Scan(v interface{}) error {
 	if value, ok := v.(time.Time); ok {
 		*d = Datetime(value)
-
 		return nil
 	}
-
 	return fmt.Errorf("can not convert %v to timestamp", v)
 }
 
 func (d *Datetime) String() string {
-
-	return time.Time(*d).Format(time.DateTime)
+	return time.Time(*d).In(localLocation).Format(time.DateTime)
 }
 
 func (d *Datetime) Year() string {
-
-	return time.Time(*d).Format("2006")
+	return time.Time(*d).In(localLocation).Format("2006")
 }
 
 func (d *Datetime) Before(u time.Time) bool {
-
 	return time.Time(*d).Before(u)
 }
 
 func (d *Datetime) Time() time.Time {
-
-	return time.Time(*d)
+	return time.Time(*d).In(localLocation)
 }
 
 func (d *Datetime) Format(layout string) string {
-
-	return time.Time(*d).Format(layout)
+	return time.Time(*d).In(localLocation).Format(layout)
 }
