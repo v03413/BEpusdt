14644e00a5bc45907c342c6e34242c9754cdae46
v03413 <admin@qzone.work>
2026-04-23 16:01:00 +0800

    refactor: add logging for MQTT publish errors

--- app/mqtt/publish.go
+++ app/mqtt/publish.go
@@ -1,6 +1,9 @@
 package mqtt
 
-import mqtt "github.com/eclipse/paho.mqtt.golang"
+import (
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/v03413/bepusdt/app/log"
+)
 
 // Publish 发布消息。若 MQTT 未连接则静默丢弃，返回 nil。
 func Publish(topic string, qos byte, retained bool, payload interface{}) mqtt.Token {
@@ -12,5 +15,16 @@ func Publish(topic string, qos byte, retained bool, payload interface{}) mqtt.To
 		return nil
 	}
 
-	return c.Publish(topic, qos, retained, payload)
+	token := c.Publish(topic, qos, retained, payload)
+	if qos > 0 {
+		go func() {
+			token.Wait()
+			if err := token.Error(); err != nil {
+				log.Warn("MQTT publish error: " + err.Error())
+				return
+			}
+		}()
+	}
+
+	return token
 }
